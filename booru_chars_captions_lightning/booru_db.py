e9389997b9f16e75ba45049ae430073c85fc2a7e
Alex Birch <Birch-san@users.noreply.github.com>
2022-08-22 01:29:47 +0100

    queries for new schema, with extra considerations for splitting dataset into training vs validation, splitting further by worker, and ensuring that the validation datset is a deterministic random sample over all the dataset shards that currently exist. the idea is that if you did fine-tuning later: you could insert new data into booru-chars.db, but under a new shard, so as not to influence which records were chosen for producing validation dataset.

--- booru_chars_captions_lightning/booru_db.py
+++ booru_chars_captions_lightning/booru_db.py
@@ -1,125 +1,192 @@
 from __future__ import annotations
-from sqlite3 import Cursor
-from typing import Iterator, NamedTuple, List, Optional, Iterable
+from sqlite3 import Connection, Cursor
+from typing import Iterator, NamedTuple, List, Optional, Iterable, Union, TypedDict
+from typing_extensions import TypeAlias
 from enum import IntEnum
 from dataclasses import dataclass
+
+_Queryable: TypeAlias = Union[Cursor, Connection]
 
 class TagCategory(IntEnum):
   GENERAL = 0
   ARTIST = 1
-  # no examples had TAG_CAT 2
-  # yeah TAG_CAT 3 and 4 both appear to represent franchise
-  FRANCHISE_0 = 3
-  FRANCHISE_1 = 4
+  # no examples had TAG_CAT 2 (perhaps category 2 is 'meta', for tags like 'animated')
+  COPYRIGHT = 3
+  CHARACTER = 4
 
   @classmethod
   def parse(cls: TagCategory, category: int) -> Optional[TagCategory]:
     return cls(category) if category in cls._value2member_map_ else None
   
   def is_franchise(self: TagCategory) -> bool:
-    return self is self.FRANCHISE_0 or self.FRANCHISE_1
+    return self is self.COPYRIGHT or self.CHARACTER
 
 # order by MD5 to get a deterministic random, otherwise it's ordered by primary key (booru, fid)
-def get_file_ids(cur: Cursor) -> Cursor:
+def get_file_ids(cur: _Queryable) -> Cursor:
   return cur.execute("""\
-select BOORU, FID from files
-order by TORR_MD5
+select booru, fid from files
+order by torr_md5
 """)
 
-def get_file_ids_from_nth(cur: Cursor, start: int) -> Cursor:
+def get_file_ids_from_nth(cur: _Queryable, start: int) -> Cursor:
   return cur.execute("""\
-select BOORU, FID from files
-order by TORR_MD5
+select booru, fid from files
+order by torr_md5
 limit -1
 offset :offset
 """, {"offset": start})
 
-def get_first_n_file_ids(cur: Cursor, length: int) -> Cursor:
+def get_first_n_file_ids(cur: _Queryable, length: int) -> Cursor:
   return cur.execute("""\
-select BOORU, FID from files
-order by TORR_MD5
+select booru, fid from files
+order by torr_md5
 limit :limit
 """, {"limit": length})
 
 class BooruFileId(NamedTuple):
-  BOORU: str
-  FID: str
+  booru: str
+  fid: str
+
+class BooruFileIdDict(TypedDict):
+  booru: str
+  fid: str
 
 def file_ids_to_dtos(cur: Cursor) -> Iterator[BooruFileId]:
-  for BOORU, FID in cur:
-    yield BooruFileId(BOORU=BOORU, FID=FID)
+  for booru, fid in cur:
+    yield BooruFileId(booru=booru, fid=fid)
 
-def get_tags(cur: Cursor, foreign_key: BooruFileId) -> List[str]:
-  BOORU, FID = foreign_key
+def get_tags(cur: _Queryable, foreign_key: BooruFileId) -> List[str]:
+  booru, fid = foreign_key
   cur.execute("""\
-select distinct TAG from tags
-where BOORU = :BOORU
-  and FID = :FID
-order by TAG ASC
-""", {"BOORU": BOORU, "FID": FID})
+select distinct tag from tags
+where booru = :booru
+  and fid = :fid
+order by tag asc
+""", BooruFileIdDict(booru=booru, fid=fid))
   return list(map(lambda result: result[0], cur.fetchall()))
 
 @dataclass
 class TagRecord:
-  TAG: str
-  CAT: Optional[TagCategory]
+  tag: str
+  cat: Optional[TagCategory]
 
-def get_tag_records(cur: Cursor, foreign_key: BooruFileId) -> List[TagRecord]:
-  BOORU, FID = foreign_key
+def get_tag_records(cur: _Queryable, foreign_key: BooruFileId) -> List[TagRecord]:
+  booru, fid = foreign_key
   cur.execute("""\
-select TAG, TAG_CAT from tags
-where BOORU = :BOORU
-  and FID = :FID
-group by TAG
-order by TAG ASC
-""", {"BOORU": BOORU, "FID": FID})
+select tag, tag_cat from tags
+where booru = :booru
+  and fid = :fid
+group by tag
+order by tag asc
+""", BooruFileIdDict(booru=booru, fid=fid))
   return [TagRecord(
-    TAG=result[0],
-    CAT=TagCategory.parse(int(result[1])),
+    tag=result[0],
+    cat=TagCategory.parse(int(result[1])),
   ) for result in cur.fetchall()]
 
+class DatasetSplit(NamedTuple):
+  total: int
+  validation: int
+
+def get_dataset_split(
+  cur: _Queryable,
+  validation_split_coeff = 0.05,
+  validation_shards: Iterable[str] = ('Safebooru 2021a', 'Safebooru 2021b', 'Safebooru 2021c', 'Safebooru 2021d', 'Safebooru 2022a', 'Safebooru 2022b', 'manual_walfie_0'),
+) -> DatasetSplit:
+  return DatasetSplit(
+    cur.fetchone("""\
+select
+count(*) as total,
+floor(count(*) * :validation_split) as validation
+from files
+where shard in :validation_shards
+""", {
+      "validation_split": validation_split_coeff,
+      "validation_shards": validation_shards,
+    })
+  )
+
 def get_validation_fids(
-  cur: Cursor,
-  limit=128,
-  blessed_franchises: Iterable[str] = ('hololive', 'touhou')
-) -> Cursor:
-  return cur.execute("""\
-select f.BOORU, f.FID
+  cur: _Queryable,
+  validation_quantity: int,
+  validation_shards: Iterable[str] = ('Safebooru 2021a', 'Safebooru 2021b', 'Safebooru 2021c', 'Safebooru 2021d', 'Safebooru 2022a', 'Safebooru 2022b', 'manual_walfie_0'),
+  rank = 0,
+  workers = 1,
+) -> Iterator[BooruFileId]:
+  shard_size: float = validation_quantity / workers
+  cur.execute("""\
+select f.booru, f.fid
 from files f
+where shard in :validation_shards
+order by torr_md5
+limit :limit
+offset :offset
+""", {
+    "limit": int(shard_size),
+    "offset": int(shard_size * rank),
+    "validation_shards": validation_shards,
+  })
+  return file_ids_to_dtos(cur)
+
+def get_recognisable_validation_fids(
+  cur: _Queryable,
+  validation_quantity: int,
+  example_quantity: int,
+  validation_shards: Iterable[str] = ('Safebooru 2021a', 'Safebooru 2021b', 'Safebooru 2021c', 'Safebooru 2021d', 'Safebooru 2022a', 'Safebooru 2022b', 'manual_walfie_0'),
+  blessed_franchises: Iterable[str] = ('hololive', 'touhou'),
+) -> Iterator[BooruFileId]:
+  cur.execute("""\
+select f.booru, f.fid
+from (
+  select f2.booru, f2.fid
+  from files f2
+  where f2.shard in :validation_shards
+  order by torr_md5
+  limit :validation_quantity
+) f
 where exists(
   select null
-   from tags t
-  where f.BOORU = t.BOORU
-    and f.FID = t.FID
-    and t.TAG IN :BLESSED_FRANCHISES
+    from tags t
+  where f.booru = t.booru
+    and f.fid = t.fid
+    and t.tag in :blessed_franchises
 )
-limit :LIMIT
+limit :example_quantity
 """, {
-    "LIMIT": limit,
-    "BLESSED_FRANCHISES": blessed_franchises
+    "validation_quantity": validation_quantity,
+    "validation_shards": validation_shards,
+    "example_quantity": min(validation_quantity, example_quantity),
+    "blessed_franchises": blessed_franchises
   })
+  return file_ids_to_dtos(cur)
 
-def get_test_fids(
-  cur: Cursor,
-  validation_limit=128,
-  blessed_franchises: Iterable[str] = ('hololive', 'touhou')
-) -> Cursor:
+
+def get_train_fids(
+  cur: _Queryable,
+  train_quantity: int,
+  validation_quantity: int,
+  validation_shards: Iterable[str] = ('Safebooru 2021a', 'Safebooru 2021b', 'Safebooru 2021c', 'Safebooru 2021d', 'Safebooru 2022a', 'Safebooru 2022b', 'manual_walfie_0'),
+  rank = 0,
+  workers = 1,
+) -> Iterator[BooruFileId]:
+  shard_size: float = train_quantity / workers
   return cur.execute("""\
-select f.BOORU, f.FID
+select f.booru, f.fid
 from files f
-where (f.BOORU, f.FID) not in (
-  select f2.BOORU, f2.FID
+where (f.booru, f.fid) not in (
+  select f2.booru, f2.fid
   from files f2
-  where exists(
-    select null
-     from tags t
-    where f2.BOORU = t.BOORU
-      and f2.FID = t.FID
-      and t.TAG IN :BLESSED_FRANCHISES
-  )
-  limit :VALIDATION_LIMIT
+  where f2.shard in :validation_shards
+  order by torr_md5
+  limit :validation_limit
 )
+order by torr_md5
+limit :limit
+offset :offset
 """, {
-    "VALIDATION_LIMIT": validation_limit,
-    "BLESSED_FRANCHISES": blessed_franchises
+    "validation_limit": validation_quantity,
+    "validation_shards": validation_shards,
+    "limit": int(shard_size),
+    "offset": int(shard_size * rank)
   })
+  return file_ids_to_dtos(cur)
--- booru_chars_captions_lightning/db.py
+++ booru_chars_captions_lightning/db.py
@@ -1,6 +1,25 @@
 import sqlite3
 from sqlite3 import Connection
+import functools as ft
+
+@ft.lru_cache(None)
+def get_sqlite3_thread_safety():
+  # Map value from SQLite's THREADSAFE to Python's DBAPI 2.0
+  # threadsafety attribute.
+  sqlite_threadsafe2python_dbapi = {0: 0, 2: 1, 1: 3}
+  conn = sqlite3.connect(":memory:")
+  threadsafety = conn.execute("""
+select * from pragma_compile_options
+where compile_options like 'THREADSAFE=%'
+""").fetchone()[0]
+  conn.close()
+
+  threadsafety_value = int(threadsafety.split("=")[1])
+
+  return sqlite_threadsafe2python_dbapi[threadsafety_value]
 
 def create_connection(db_file: str) -> Connection:
   """ create a database connection to a SQLite database """
-  return sqlite3.connect(db_file)
+  # https://ricardoanderegg.com/posts/python-sqlite-thread-safety/
+  check_same_thread=get_sqlite3_thread_safety() is not 3
+  return sqlite3.connect(db_file, check_same_thread=check_same_thread)
